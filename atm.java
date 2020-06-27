5dd99bd9e859126be51d6f3d61d721284b617713
Programlog <66264617+Programlog@users.noreply.github.com>
2020-06-26 21:05:51 -0400

    Update atm.java

--- atm.java
+++ atm.java
@@ -1,32 +1,45 @@
-//hello test push
 import java.util.*;
+
 class Main {
     public static void main(String [] args){
         Scanner keyboard = new Scanner(System.in);
         double balance = 10000.00;
         String userChoice;
+        boolean running = true;
+        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-        System.out.print("Type 1 for Withdraw"
-                + "\nType 2 for Deposit" + "\nType 3 for Check Balance"
-                + "\nChoose 4 for Exit" + "\nChoose the operation you want to perform: ");
-        userChoice = keyboard.nextLine();
+        while (running == true) {
 
-        if(userChoice.equals("1")){
-            System.out.print("How much money(dollars) do you want to withdraw: ");
-            double userWithdraw = keyboard.nextDouble();
-            balance -= userWithdraw;
-            System.out.printf("You withdrew $%.2f", userWithdraw);
+            System.out.print("Type 1 for Withdraw"
+                    + "\nType 2 for Deposit" + "\nType 3 for Check Balance"
+                    + "\nType 4 to Exit" + "\nChoose the operation you want to perform: ");
+            userChoice = keyboard.nextLine();
 
-        } else if(userChoice.equals("2")){
-            System.out.print("How much money do you want to deposit: ");
-            double userDeposit = keyboard.nextDouble();
-            balance += userDeposit;
-            System.out.printf("You have successfully deposited $%.2f", userDeposit);
+            if(userChoice.equals("1")){
+                System.out.print("How much money(dollars) do you want to withdraw: ");
+                double userWithdraw = keyboard.nextDouble();
+                balance -= userWithdraw;
+                System.out.printf("You withdrew $%.2f", userWithdraw);
+                
+            } else if(userChoice.equals("2")){
+                System.out.print("How much money do you want to deposit: ");
+                double userDeposit = keyboard.nextDouble();
+                balance += userDeposit;
+                System.out.printf("You have successfully deposited $%.2f", userDeposit);
 
-        } else if(userChoice.equals("3")){
-            System.out.print("Your current balance is ", balance);
+            } else if(userChoice.equals("3")){
+                System.out.println("\n\n");
+                System.out.printf("*Your balance is $%.2f", balance);
+
+            } else if(userChoice.equals("4")){
+                System.out.print("Exiting...\n");
+                running = false;
+                return;
+
+            }
+
+            System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
         }
-
     }
 }
